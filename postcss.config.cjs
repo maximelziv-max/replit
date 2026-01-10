73ce27fc2d1179597778f0bf77e19baa66454455
maximelziv-max <maximelziv@gmail.com>
2026-01-10 15:00:54 +0000

    fix: clean postcss config (remove merge artifacts)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
-=======
-export default {
->>>>>>> a0912c2 (fix: postcss config compatible with ES modules)
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> a0912c2 (fix: postcss config compatible with ES modules)
-  module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
